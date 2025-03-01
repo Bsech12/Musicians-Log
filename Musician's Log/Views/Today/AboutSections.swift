1833957d08a9f33344a7e24358bc18a5062b4302
Bsech12 <bryce.sechrist@gmail.com>
2025-03-01 15:54:56 -0800

    A few more finishing touches: - Added App Image - Added "About" section in settings - Fixed timer/difference between timers - Fixed some slideover problems in History

---
+++ Musician's Log/Views/Today/AboutSections.swift
@@ -0,0 +1,18 @@
+//
+//  AboutSections.swift
+//  Musician's Log
+//
+//  Created by Bryce Sechrist on 3/1/25.
+//
+
+import SwiftUI
+
+struct AboutSections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutSections()
+}
